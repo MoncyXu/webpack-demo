2551419dc0b3c57d5c0fb402e81c943dbe6bb2b6
Mx <deeven.xu@maxnerva.com>
2019-12-25 23:44:48 +0800

    update

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import _ from "lodash";
 import "./style.css";
+import ImageSnow from "./snow.png";
+import Data from "./data.xml";
 
 function component() {
   var element = document.createElement("div");
@@ -7,6 +9,13 @@ function component() {
   // Lodash（目前通过一个 script 脚本引入）对于执行这一行是必需的
   element.innerHTML = _.join(["Hello", "webpack"], " ");
   element.classList.add("hello");
+
+  // 将图像添加到我们现有的div
+  var myImage = new Image();
+  myImage.src = ImageSnow;
+  element.appendChild(myImage);
+
+  console.log(Data);
 
   return element;
 }
