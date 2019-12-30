15f15b3552a2c932926a06c27145a8fce2bb8760
Mx <deeven.xu@maxnerva.com>
2019-12-30 23:57:37 +0800

    update

--- src/print.js
+++ src/print.js
@@ -1,3 +1,5 @@
 export default function printMe() {
   console.log("I get called from print.js!");
+  // console.error("Something unexpect happend!");
+  alert("Oh!It's awesome!!");
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
-const {CleanWebpackPlugin} = require("clean-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 module.exports = {
   entry: {
@@ -9,7 +9,12 @@ module.exports = {
   },
   output: {
     filename: "[name].bundle.js",
-    path: path.resolve(__dirname, "dist")
+    path: path.resolve(__dirname, "dist"),
+    publicPath: "/"
+  },
+  devtool: "inline-source-map",
+  devServer: {
+    contentBase: "./dist"
   },
   module: {
     rules: [
